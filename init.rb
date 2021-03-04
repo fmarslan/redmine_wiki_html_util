d6503e4f232d3a7153d46238f4dc2b8ec16509e4
Fatih Mehmet ARSLAN <iletisim@fmarslan.com>
2021-03-04 08:44:16 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -16,15 +16,9 @@ Redmine::Plugin.register :redmine_wiki_html_util do
     macro :html, :parse_args => false do |obj, args, text|
       page = obj.page
       raise 'Page not found' if page.nil?
-
-      # For security, only allow insertion on protected (locked) wiki pages
-      if page.protected
-        content = text || args 
-        result = "#{ CGI::unescapeHTML(content) }".html_safe
-	return result
-      else
-        return "<!-- Macro removed due to wiki page being unprotected -->"
-      end
+      content = text || args 
+      result = "#{ CGI::unescapeHTML(content) }".html_safe
+      return result
     end 
   end
         
@@ -33,16 +27,10 @@ Redmine::Plugin.register :redmine_wiki_html_util do
     macro :css, :parse_args => false do |obj, args, text|
       page = obj.page
       raise 'Page not found' if page.nil?
-
-      # For security, only allow insertion on protected (locked) wiki pages
-      if page.protected 
-        content = text || args 
-        content = "<style type=\"text/css\">"+content+"</style>"        
-        result = "#{ CGI::unescapeHTML(content) }".html_safe
-        return result
-      else
-        return "<!-- Macro removed due to wiki page being unprotected -->"
-      end
+      content = text || args 
+      content = "<style type=\"text/css\">"+content+"</style>"        
+      result = "#{ CGI::unescapeHTML(content) }".html_safe
+      return result
     end 
   end
         
@@ -51,15 +39,9 @@ Redmine::Plugin.register :redmine_wiki_html_util do
     macro :css_url do |obj, args|
       page = obj.page
       raise 'Page not found' if page.nil?
-
-      # For security, only allow insertion on protected (locked) wiki pages
-      if page.protected 
-        result = "<script> var head = document.getElementsByTagName('head')[0], t = document.createElement('link'); t.href = "+args[0]+"; t.media='all'; t.rel='stylesheet'; head.appendChild(t); </script>"
-        result = "#{ CGI::unescapeHTML(result) }".html_safe
-	return result
-      else
-        return "<!-- Macro removed due to wiki page being unprotected -->"
-      end
+      result = "<script> var head = document.getElementsByTagName('head')[0], t = document.createElement('link'); t.href = "+args[0]+"; t.media='all'; t.rel='stylesheet'; head.appendChild(t); </script>"
+      result = "#{ CGI::unescapeHTML(result) }".html_safe
+      return result
     end 
   end
         
@@ -68,16 +50,10 @@ Redmine::Plugin.register :redmine_wiki_html_util do
     macro :js, :parse_args => false do |obj, args, text|
       page = obj.page
       raise 'Page not found' if page.nil?
-
-      # For security, only allow insertion on protected (locked) wiki pages
-      if page.protected
-        content = text || args 
-        content = "<script>"+content+"</script>"
-        result = "#{ CGI::unescapeHTML(content) }".html_safe
-	return result
-      else
-        return "<!-- Macro removed due to wiki page being unprotected -->"
-      end
+      content = text || args 
+      content = "<script>"+content+"</script>"
+      result = "#{ CGI::unescapeHTML(content) }".html_safe
+      return result
     end 
   end
         
@@ -85,16 +61,10 @@ Redmine::Plugin.register :redmine_wiki_html_util do
     desc "Insert a JS file into the DOM\nUsage:\n<pre>{{js_url(http://js.url)}}</pre>"
     macro :js_url do |obj, args|
       page = obj.page
-      raise 'Page not found' if page.nil?
-
-      # For security, only allow insertion on protected (locked) wiki pages
-      if page.protected 
-        result = "<script> var head = document.getElementsByTagName('head')[0], t = document.createElement('script'); t.src = "+args[0]+"; t.type='text/javascript'; head.appendChild(t); </script>"
-	result = "#{ CGI::unescapeHTML(result) }".html_safe        
-	return result
-      else
-        return "<!-- Macro removed due to wiki page being unprotected -->"
-      end
+      raise 'Page not found' if page.nil? 
+      result = "<script> var head = document.getElementsByTagName('head')[0], t = document.createElement('script'); t.src = "+args[0]+"; t.type='text/javascript'; head.appendChild(t); </script>"
+      result = "#{ CGI::unescapeHTML(result) }".html_safe        
+      return result
     end 
   end
         
